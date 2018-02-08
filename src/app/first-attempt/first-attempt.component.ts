7303b87dd16a2e6c0949c5cac6c798133f9abc53
DenNazgul <nazguldragon@mail.ru>
2018-02-08 19:41:14 +0300

    interim result

--- src/app/first-attempt/first-attempt.component.ts
+++ src/app/first-attempt/first-attempt.component.ts
@@ -9,17 +9,54 @@ import * as _ from 'lodash';
 export class FirstAttemptComponent {
   title = 'shit parad';
   valueFromInput = 'g';
+  editMode = '';
+  valueFromChangedInput = 'введите текст'
   todos = [
     'Первая строка',
     'Второй пункт',
     'Кажется, работает',
     'Точно!',
-    ];
-    onClick () {
-     // if(!_.isEmpty(this.valueFromInput)) {
-        this.todos.push(this.valueFromInput);
-     // }
+  ];
+  onClick() {
+
+    if (!_.isEmpty(this.valueFromInput)) {
+      this.todos.push(this.valueFromInput);
     }
+  }
+  onDelete(todo: string) {
+    const index = this.todos.indexOf(todo);
+    if (index > -1) {
+
+      this.todos.splice(index, 1);
+    }
+  }
+  onDeleteIndex(index: number) {
+    this.todos.splice(index, 1);
+  }
+  onDeleteLodash(todo: string) {
+    _.remove(this.todos, item => {
+      return item === todo;
+    })
+  }
+  onSave(todo: string) {
+    const index = this.todos.indexOf(todo);
+    this.todos.splice(index, 1, this.valueFromChangedInput);
+    this.valueFromChangedInput = '';
+    this.editMode = '';
+  }
+  blaster() {
+    if (!_.isEmpty(this.valueFromInput)) {
+      if (!_.isEmpty(this.editMode)) {
+        const index = this.todos.indexOf(this.editMode);
+        this.todos.splice(index, 1, this.valueFromInput);
+        this.valueFromInput = '';
+        this.editMode = '';
+      }
+      else {
+        this.todos.push(this.valueFromInput);
+      }
+    }
+  }
 }
 /*export class FirstAttemptComponent implements OnInit {
 
