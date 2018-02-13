5cbcbcd4f7cbe1eaf20acd18357d56c78a33dee7
DenNazgul <nazguldragon@mail.ru>
2018-02-13 13:38:13 +0300

    final visual and optimisation fixes

--- src/app/first-attempt/first-attempt.component.ts
+++ src/app/first-attempt/first-attempt.component.ts
@@ -14,15 +14,9 @@ export class FirstAttemptComponent {
   idForChange: number;
   valueFromChangedInput = 'введите текст'
   todos: arrayModel[] = [];
-  /*[
-    'Первая строка',
-    'Второй пункт',
-    'Кажется, работает',
-    'Точно!',
-  ];*/
   onClick() {
     if (!_.isEmpty(this.valueFromInput)) {
-      const newElement = new arrayModel(); //что конкретно мы делаем этой строкой
+      const newElement = new arrayModel();
       newElement.id = this.getNextId();
       newElement.title = this.valueFromInput;
       this.todos.push(newElement);
@@ -33,35 +27,13 @@ export class FirstAttemptComponent {
     return _.isUndefined(elementWithMaxId) ? 0 : elementWithMaxId.id + 1;
   }
   getIdForChange(arrayModel: arrayModel) {
-    const currentElementId = arrayModel.id;
-    this.idForChange = currentElementId;
+    this.idForChange = arrayModel.id;
   }
   onDelete(arrayModel: arrayModel) {
     _.remove(this.todos, (todo1: arrayModel) => {
       return todo1.id === arrayModel.id;
     })
   }
-  onDeleteIndex(index: number) {
-    this.todos.splice(index, 1);
-  }
-  onDeleteLodash(arrayModel: string) {
-    _.remove(this.todos, item => {
-      return item === arrayModel;
-    })
-  }
-  /*onDelete(todo: string) {
-    const index = this.todos.indexOf(todo);
-    if (index > -1) {
-
-      this.todos.splice(index, 1);
-    }
-  }*/
-  /*onSave(todo: string) {
-    const index = this.todos.indexOf(todo);
-    this.todos.splice(index, 1, this.valueFromChangedInput);
-    this.valueFromChangedInput = '';
-    this.editMode = '';
-  }*/
   blaster() {
     if (!_.isEmpty(this.valueFromInput)) {
       if (!_.isEmpty(this.editMode)) {
@@ -71,14 +43,9 @@ export class FirstAttemptComponent {
         this.todos.splice(changedElement.id, 1, changedElement);
         this.valueFromInput = '';
         this.editMode = '';
-
-        /*const index = this.todos.indexOf(this.editMode);
-        this.todos.splice(index, 1, this.valueFromInput);
-        this.valueFromInput = '';
-        this.editMode = '';*/
       }
       else {
-      const newElement = new arrayModel(); //что конкретно мы делаем этой строкой
+      const newElement = new arrayModel();
       newElement.id = this.getNextId();
       newElement.title = this.valueFromInput;
       this.todos.push(newElement);
